bb86230c6d0206f36048b2b4f817a019fef765ac
jmattaa <mattajonathan1@gmail.com>
2025-06-27 16:14:58 +0200

    start a full rewrite

--- assemble.sh
+++
@@ -1,5 +0,0 @@
-read -p "asm file name without ext: " filename
-filename_S=${filename}.S 
-filename_o=${filename}.o
-as --32 ${filename_S} -o ${filename_o}
-ld ${filename_o} -o ${filename} -melf_i386
--- src/main.c
+++ src/main.c
@@ -1,36 +1,7 @@
-#include "IO.h"
-#include "jlang.h"
 #include <stdio.h>
 
-int main(int argc, char** argv)
+int main (int argc, char **argv) 
 {
-    if (argc < 2) {
-        printf("jlang usage: %s <file>\n", argv[0]);
-        return 1;
-    }
-
-    int compiled = 0;
-    for (int i = 0; i < argc; i++) {
-        if (argv[i][0] == '-') {
-            switch (argv[i][1]) {
-            case 'o': {
-                char* src = IO_ReadFile(argv[1]);
-                Jlang_Compile(src, argv[i + 1]);
-                compiled = 1;
-                break;
-            }
-            case 'h':
-                printf("jlang usage: %s <file> -o <output>\n", argv[0]);
-                compiled = 1; // we dont want to compile
-                break;
-            }
-        }
-    }
-
-    if (!compiled) {
-        printf("jlang: No output file provided\n");
-        printf("jlang usage: %s <file> -o <output>\n", argv[0]);
-    }
-
+    printf("Hello Jlang!\n");
     return 0;
 }
